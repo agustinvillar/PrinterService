865c3c8567dce9bc122214c82f7b5734efa3271e
frangerman <fespindola@morfi.com.uy>
2019-12-02 17:10:34 -0300

    Windows

---
+++ Dominio/Orders.cs
@@ -0,0 +1,44 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    [FirestoreData]
+    class Orders
+    {
+        [FirestoreProperty("address")]
+        public string Address {get; set;}        
+        [FirestoreProperty ("printed")]
+        public bool Printed { get; set; }
+        [FirestoreProperty("bookingId")]
+        public int BookingId { get; set; }
+        [FirestoreProperty("items")]
+        public  Item[] Items { get; set; }
+        [FirestoreProperty("madeAt")]
+        public string MadeAt { get; set; }
+        [FirestoreProperty("orderDate")]
+        public string OrderDate { get; set; }
+        [FirestoreProperty("orderNumber")]
+        public double OrderNumber { get; set; }
+        [FirestoreProperty("orderType")]
+        public string OrderType { get; set; }
+        [FirestoreProperty("status")]
+        public string Status { get; set; }
+        [FirestoreProperty("tableOpeningId")]
+        public string TableOpeningId { get; set; }
+        [FirestoreProperty("takeAwayHour")]
+        public string TakeAwayHour { get; set; }
+        [FirestoreProperty("total")]
+        public double Total { get; set; }
+        [FirestoreProperty("updatedAt")]
+        public double UpdatedAt { get; set; }
+        [FirestoreProperty("userId")]
+        public string UserId { get; set; }
+        [FirestoreProperty("userName")]
+        public string UserName { get; set; }
+    }
+}
---
+++ MenooPrinterService/Ajustes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenooPrinterService
+{
+    public partial class Ajustes : Form
+    {
+        private string[] Printers;
+
+        public Ajustes()
+        {
+            InitializeComponent();
+            Init();
+        }
+        private void Init()
+        {
+            var printers = new List<string>();
+            foreach (string printer in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+                printers.Add(printer);
+            Printers = printers.ToArray();
+
+            this.DdlImpresoras.DataSource = Printers;
+
+            this.DdlImpresoras.SelectedIndex = this.DdlImpresoras.FindStringExact(ConfigurationManager.AppSettings["PrinterName"]);
+            this.TxtRestoId.Text = ConfigurationManager.AppSettings["StoreID"];
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+
+                string printerName = config.AppSettings.Settings["PrinterName"].Value;
+                config.AppSettings.Settings["PrinterName"].Value = this.DdlImpresoras.SelectedValue.ToString();
+
+                string storeID = config.AppSettings.Settings["StoreID"].Value;
+                config.AppSettings.Settings["StoreID"].Value = this.TxtRestoId.Text;
+
+                config.Save(ConfigurationSaveMode.Modified);
+
+                MessageBox.Show("Guardado OK", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
