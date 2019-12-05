46adb6f8c9d6dd70619899c5403f85b37ea135b1
JBauzan <54155969+JBauzan@users.noreply.github.com>
2019-12-05 10:23:35 -0300

    UpdateAddedBookingPrintingAndTableOpeningPrinting

---
+++ Dominio/User.cs
@@ -0,0 +1,16 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    [FirestoreData]
+    class User
+    {
+        [FirestoreProperty("name")]
+        public string Name { get; set; }
+    }
+}
