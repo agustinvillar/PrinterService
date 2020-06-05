496e70f60164fc269c3c5e11189cd75ceb7cb053
frangerman <fespindola@menoo.com.uy>
2020-06-05 19:23:21 -0300

    Update Firebase.cs

--- Dominio/Firebase.cs
+++ Dominio/Firebase.cs
@@ -14,19 +14,22 @@ using System.Configuration;
 using System.Drawing.Printing;
 using System.Runtime.CompilerServices;
 using System.Threading;
+using Google;
+using System.Reflection;
+using System.Security.Cryptography;
 
 namespace Dominio
 {
     public static class Firebase
     {
-        private static FirestoreDb _db = AccessDatabase();
-        private static string _printerName = ConfigurationManager.AppSettings["PrinterName"];
+        private static string _printerName;
+        private static FirestoreDb _db;
         private static FirestoreChangeListener _lastTableOpeningListener;
         private static FirestoreChangeListener _orderFamilyListener;
         private static FirestoreChangeListener _bookingListener;
         private static FirestoreChangeListener _tableOpenings;
 
-        private static FirestoreDb AccessDatabase()
+        private static FirestoreDb AccessDatabaseProduction()
         {
             var credential = GoogleCredential.FromJson("{'type':'service_account'," +
                                                        "'project_id':'comemosya'," +
@@ -43,6 +46,28 @@ namespace Dominio
             var client = FirestoreClient.Create(channel);
             return FirestoreDb.Create("comemosya", client);
         }
+        private static FirestoreDb AccessDatabaseTESTING()
+        {
+            var credential = GoogleCredential.FromJson("{'type':'service_account'," +
+                                                       "'project_id':'menootest-9fa5a'," +
+                                                       "'private_key_id':'26339e76106194376a318dcb8d8e588b5cef6a78'," +
+                                                       "'private_key':'-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDoE0F5Tw5Y/223\nJ5Kpvlg7jbkAwPzy97OMV+AHFOvm8/+ZemQBxIWof+cbS654R0R9HGilFXUF4v/b\nKdchnQN7ZLKAtL16CMEQgBTWTDytbaZWEexWOt7w8itzeQ1+jvKSKgoMvOu5/k+O\nPLo6UyIQQQFcWoBW8NVeM8cx1FsACTjJBUIh6CXDUFS8ze+hbMQEmnhLscD3ZMJR\n+nhJSj2O6GVPAn34DTZRxv9LupqfGzZF2JJLz8tCtSs+aNSyUYIrVV+jYVUQ93xJ\nsufkdn4MAFJ6fVgw8/3BPYjwpSsuYUbshbuc20xJSYjZxoCUngM7Yn03GVsqO/Rp\nYya87AEJAgMBAAECggEAAmjkxqYv/3OTh5HVH4cW8nNbxuq6FanFxwDIljo84taI\nwma3cB9Cxgeh9jIYey4+Q1BOs9wfrXJ4dqWeEr7HIPpgMh7uUryRiKLT1I/RF3nq\nfr1L529QDk9tbRMGNVi1oxflp9E0X2eJGvB9fIqNcX7DTVqxN3XjuUkvWdCbK0po\nKS6Vly1qgpBW5vlyKq4IbAeTDh2uoja352G6G0Yjjczo8lBQeflXQ+A/VQhex/I9\nszCYiW6ZdwVlwxN2zXbpkzy0C0U2rkrXy5dzv3pMlX1IaIuqKlI6ehUQXwNHsPvA\n8/4PuUSbSGcuQLLcpg8FVO6a92EteZyoPY7Y1UltQQKBgQD05bLd5eqoBCaGDqBu\nsFzZGZvZZaAIFfl2Helz7QopF9HZynytXS7jvyFAGsYVNZ0/hltRxOCmp1YLZtBI\nseV9SAFdF+ShGbfTokyYRa3qTM4jcEvfmJiSW5Q0IMEDG+UqB5JmKoRd+YQLzieB\nviEXUXkxXpFranck9b1ZSMXZcQKBgQDymL5BttCnUuBGMSZ9slnNLNR0gVka6xFX\n0+ZSy+M5iwo5zKdgA7UKEuzE6rgGI7Mj6tQRTeO1NUljALQ+KAePdZAYX8BvrzLV\n0mF7GNc5ulsKDJxrPtzn/RBtDDangLr2DcMZggP2BRxUpmG7t5OIzeiVBW6ZHZRW\nPICZrTuVGQKBgQCYmFfvtEeXEZ7/gTWuQu5XyIE34P7qiua6FsFUnqrqGBGGZ4lw\nbNO+zWVmkEhFBvdIkets9AQXU8VlrVazNUYN3kQbQbwQNfo5QLQBXcmUaO85Xcup\nM2g+KhoasR4TVdphaf5q8qsv8z24LWioi1QLN5UQkiCCkgBTY1vsuk+twQKBgExs\nvhMpqpXrz+eM+FlE5HF0nAGP9ig6wZ3vjXGr9YtdN/15cYkX4eKoj5qBbzPP71Fz\nWxeQeBnQDax4vk+OgMM7AAgNsiv8/4DI5BjJfJQdFy0VR/mpNiKHYLNZ06X1MfDt\n6PaSNPk+Juyr9cITVREV/R1lNrBZ1y9LpB/FqS2RAoGBAJIItGLuubQajkcS3uLX\nC169DyaBlzxH1SHrdtAaZM66BtViD5uvNrJW6AsX33yNVAtpCoPP6XPtavIrQ4Rm\nZh2QjblrYDuNGEFNuKv1kVDUS5oHkw1Al87IwT5badRnEm12hm3X92kRMu0AZORk\n4XtL2zupPadZXhzo1bBILIfh\n-----END PRIVATE KEY-----\n'," +
+                                                       "'client_email':'menootest-9fa5a@appspot.gserviceaccount.com'," +
+                                                       "'client_id': '108009182778403042502'," +
+                                                       "'auth_uri':'https://accounts.google.com/o/oauth2/auth'," +
+                                                       "'token_uri':'https://oauth2.googleapis.com/token'," +
+                                                       "'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs'," +
+                                                       "'client_x509_cert_url':'https://www.googleapis.com/robot/v1/metadata/x509/menootest-9fa5a%40appspot.gserviceaccount.com'}");
+            var channelCredentials = credential.ToChannelCredentials();
+            var channel = new Channel(FirestoreClient.DefaultEndpoint.ToString(), channelCredentials);
+            var client = FirestoreClient.Create(channel);
+            return FirestoreDb.Create("menootest-9fa5a", client);
+        }
+        private static void Init()
+        {
+            _printerName = ConfigurationManager.AppSettings["PrinterName"];
+            _db = AccessDatabaseTESTING();
+        }
         private static void StartListen(string storeId)
         {
             BookingsListen(storeId);
@@ -53,6 +78,7 @@ namespace Dominio
         {
             return Task.Run(() =>
             {
+                Init();
                 var storeId = ConfigurationManager.AppSettings["StoreID"];
 
                 if (string.IsNullOrEmpty(storeId))
@@ -61,7 +87,6 @@ namespace Dominio
                 StartListen(storeId);
             });
         }
-
         public static async Task RefreshListener(string storeId)
         {
             await _orderFamilyListener.StopAsync();
@@ -71,6 +96,7 @@ namespace Dominio
             StartListen(storeId);
         }
 
+        #region CLOSE TABLE OPENING FAMILY
         private static void TableOpeningsListen(string storeId)
         {
             _tableOpenings = _db.Collection("tableOpeningFamily")
@@ -81,27 +107,44 @@ namespace Dominio
         {
             try
             {
-                foreach (var snap in snapshot)
-                {
-                    var to = snap.ConvertTo<TableOpeningFamily>();
+                List<TableOpeningFamily> tableOpeningFamilies = snapshot.Documents
+                                                                .Select(d => d.ConvertTo<TableOpeningFamily>())
+                                                                .ToList();
+                foreach (var to in tableOpeningFamilies)
                     if (to.Closed)
-                    {
-                        PrintDocument pd = new PrintDocument();
-                        pd.PrinterSettings.PrinterName = _printerName;
-                        pd.PrintPage += (sender, args) => PrintCloseTableFamily(to, sender, args);
-                        pd.Print();
-                    }
-                }
+                        PrintCloseTableFamily(to);
             }
             catch (Exception ex)
             {
                 LogErrorAsync(ex.Message);
             }
         };
+        private static void PrintCloseTableFamily(TableOpeningFamily tableOpening)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrinterSettings.PrinterName = _printerName;
+            pd.PrintPage += (sender, args) => PrintCloseTableFamily(tableOpening, sender, args);
+            pd.Print();
+        }
         private static void PrintCloseTableFamily(TableOpeningFamily tableOpening, object sender, PrintPageEventArgs args)
         {
+            var printFont = new Font("Arial", 13);
+            float yPos;
+            float xPos = 20;
+            int count = 0;
+            float leftMargin = args.MarginBounds.Left;
+            float topMargin = args.MarginBounds.Top;
+            string line = string.Empty;
+            if (tableOpening.Closed)
+            {
+                line = "Mesa cerrada" + "\n" + "\n" + "Número de mesa: " + tableOpening.TableNumberId + "\n" + "\n" +
+                              "Fecha: " + tableOpening.ClosedAt + "\n" + "\n" + "Total: $ " + tableOpening.TotalToPay;
+            }
 
+            yPos = topMargin + (count * printFont.GetHeight(args.Graphics));
+            args.Graphics.DrawString(line, printFont, Brushes.Black, xPos, yPos, new StringFormat());
         }
+        #endregion
 
         #region ORDER FAMILY
         private static void PrintOrder(Orders orden, object sender, PrintPageEventArgs args)
@@ -184,17 +227,20 @@ namespace Dominio
             try
             {
                 Orders orden = snapshot.Documents.Single().ConvertTo<Orders>();
-
-                PrintDocument pd = new PrintDocument();
-                pd.PrinterSettings.PrinterName = _printerName;
-                pd.PrintPage += (sender, args) => PrintOrder(orden, sender, args);
-                pd.Print();
+                PrintOrder(orden);
             }
             catch (Exception ex)
             {
                 LogErrorAsync(ex.Message);
             }
         };
+        private static void PrintOrder(Orders order)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrinterSettings.PrinterName = _printerName;
+            pd.PrintPage += (sender, args) => PrintOrder(order, sender, args);
+            pd.Print();
+        }
         #endregion
 
         #region LAST TABLE OPENING
@@ -210,16 +256,20 @@ namespace Dominio
             try
             {
                 TableOpeningFamily to = snapshot.Documents.Single().ConvertTo<TableOpeningFamily>();
-                PrintDocument pd = new PrintDocument();
-                pd.PrinterSettings.PrinterName = _printerName;
-                pd.PrintPage += (sender, args) => PrintLastTableOpening(to, sender, args);
-                pd.Print();
+                PrintLastTableOpening(to);
             }
             catch (Exception ex)
             {
                 LogErrorAsync(ex.Message);
             }
         };
+        private static void PrintLastTableOpening(TableOpeningFamily to)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrinterSettings.PrinterName = _printerName;
+            pd.PrintPage += (sender, args) => PrintLastTableOpening(to, sender, args);
+            pd.Print();
+        }
         private static void PrintLastTableOpening(TableOpeningFamily tableOpening, object sender, PrintPageEventArgs args)
         {
             var printFont = new Font("Arial", 13);
@@ -229,16 +279,9 @@ namespace Dominio
             float leftMargin = args.MarginBounds.Left;
             float topMargin = args.MarginBounds.Top;
             string line;
-            if (tableOpening.Closed)
-            {
-                line = "Mesa cerrada" + "\n" + "\n" + "Número de mesa: " + tableOpening.TableNumberId + "\n" + "\n" +
-                              "Fecha: " + tableOpening.ClosedAt + "\n" + "\n" + "Total: $ " + tableOpening.TotalToPay;
-            }
-            else
-            {
-                line = "Apertura de mesa" + "\n" + "\n" + "Número de mesa: " + tableOpening.TableNumberId + "\n" + "\n" +
-                       "Fecha: " + tableOpening.OpenedAt + "\n" + "\n" + "Número de Personas: " + tableOpening.ActiveGuestQuantity;
-            }
+
+            line = "Apertura de mesa" + "\n" + "\n" + "Número de mesa: " + tableOpening.TableNumberId + "\n" + "\n" +
+                   "Fecha: " + tableOpening.OpenedAt + "\n" + "\n" + "Número de Personas: " + tableOpening.ActiveGuestQuantity;
 
             yPos = topMargin + (count * printFont.GetHeight(args.Graphics));
             args.Graphics.DrawString(line, printFont, Brushes.Black, xPos, yPos, new StringFormat());
@@ -246,6 +289,13 @@ namespace Dominio
         #endregion
 
         #region BOOKINGS
+        private static void PrintBooking(Booking booking, User user)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrinterSettings.PrinterName = ConfigurationManager.AppSettings["PrinterName"];
+            pd.PrintPage += (sender, args) => PrintBooking(booking, user, sender, args);
+            pd.Print();
+        }
         private static void BookingsListen(string storeId)
         {
             _bookingListener = _db.Collection("bookings")
@@ -260,22 +310,11 @@ namespace Dominio
             {
                 Booking booking = snapshot.Single().ConvertTo<Booking>();
                 User user = null;
-
                 var snapshotUser = await _db.Collection("customers").Document(booking.UserId).GetSnapshotAsync();
-
                 if (snapshotUser.Exists)
                     user = snapshotUser.ConvertTo<User>();
-
-                if (!booking.Printed)
-                {
-                    if (booking.BookingNumber.ToString().Length == 8)
-                    {
-                        PrintDocument pd = new PrintDocument();
-                        pd.PrinterSettings.PrinterName = ConfigurationManager.AppSettings["PrinterName"];
-                        pd.PrintPage += (sender, args) => PrintBooking(booking, user, sender, args);
-                        pd.Print();
-                    }
-                }
+                if (!booking.Printed && booking.BookingNumber.ToString().Length == 8)
+                    PrintBooking(booking, user);
             }
             catch (Exception ex)
             {
