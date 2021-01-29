aef58807ca4eecfd86e9b700e8806ccd9d579e59
Carlos Campos <danger572.cc@gmail.com>
2021-01-29 13:30:10 -0300

    Solución para listener de firebase

--- Menoo.PrinterService.Listener/Program.cs
+++ Menoo.PrinterService.Listener/Program.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace Menoo.PrinterService.Listener
 {
     static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
         static void Main()
         {
             ServiceBase[] ServicesToRun;
