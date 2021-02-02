b54b5c71f971dfdee5848eee2cc5800d7c48c77c
Carlos Campos <danger572.cc@gmail.com>
2021-02-02 15:18:06 -0300

    Servicio de colas y estructura básica de los mensajes

---
+++ 0-Infraestructure/Menoo.PrinterService.Infraestructure/Interfaces/IQueueService.cs
@@ -0,0 +1,13 @@
+﻿using Menoo.PrinterService.Infraestructure.Queues;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Menoo.PrinterService.Infraestructure.Interfaces
+{
+    public interface IQueueService
+    {
+        Task PublishAsync(Message data, Dictionary<string, string> extras = null);
+
+        Task RecieveAsync(Message data, Dictionary<string, string> extras = null);
+    }
+}
---
+++ 0-Infraestructure/Menoo.PrinterService.Infraestructure/Queues/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace Menoo.PrinterService.Infraestructure.Queues
+{
+    public sealed class Message
+    {
+        public string PrintEvent { get; set; }
+
+        public string DocumentId { get; set; }
+    }
+}
