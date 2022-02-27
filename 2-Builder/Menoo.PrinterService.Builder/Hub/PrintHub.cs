828090aaae3ff483e3302aed3ed88ecf567d4bec
Carlos Campos <ccampos@menoo.com.uy>
2022-02-27 02:56:22 -0300

    Métodos, para identificar clientes de impresión por restaurantes.

--- 2-Builder/Menoo.PrinterService.Builder/Hub/PrintHub.cs
+++ 2-Builder/Menoo.PrinterService.Builder/Hub/PrintHub.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Menoo.PrinterService.Builder.Hub
 {
@@ -6,14 +9,55 @@ namespace Menoo.PrinterService.Builder.Hub
     {
         private readonly IHubContext _hub;
 
+        private readonly Dictionary<string, string> _printers;
+
         public PrintHub()
         {
             _hub = GlobalHost.ConnectionManager.GetHubContext("PrintHub");
+            _printers = new Dictionary<string, string>();
+        }
+
+        public override Task OnConnected()
+        {
+            _printers.Add(Context.ConnectionId, "");
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Remove(Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        [HubMethodName("subscribe")]
+        public void SubscribeToPrinterGroup(string storeId, string clientConnectionId)
+        {
+            if (_printers.ContainsKey(clientConnectionId))
+            {
+                _printers[clientConnectionId] = storeId;
+                this.Groups.Add(clientConnectionId, storeId);
+            }
         }
 
         public void SendToPrint(string ticket, int copies, string printer)
         {
             _hub.Clients.All.recieveTicket(ticket, copies, printer);
         }
+
+        #region private methods
+        private void Remove(string connectionId)
+        {
+            lock (_printers)
+            {
+                foreach (var item in _printers)
+                {
+                    if (_printers.ContainsKey(connectionId))
+                    {
+                        _printers.Remove(item.Key);
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
